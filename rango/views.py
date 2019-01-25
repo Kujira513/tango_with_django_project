83d78676eb4994e624c6c649c0493d763175c1c4
Chung Ki Yau <2359033y@student.gla.ac.uk>
2019-01-25 15:45:27 +0000

    Chapter 4

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     context_dict = {'boldmessage': "Crunchy, creamy cookie, candy, cupcake!"}
-    return render(request, 'rango/index.html',context = context_dict)
+    return render(request, 'rango/index.html', context = context_dict)
     #return HttpResponse("Rango says hey there partner! <br/> <a href = '/rango/about/'>About</a>")
 
 def about(request):
