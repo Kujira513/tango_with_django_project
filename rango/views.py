231ba6d117dd602993fc7f644c75d8ef3a536c1c
Chung Ki Yau <2359033y@student.gla.ac.uk>
2019-02-07 16:48:57 +0000

    end of chapter 7

--- rango/views.py
+++ rango/views.py
@@ -78,17 +78,17 @@ def add_page(request, category_name_slug):
         category = Category.objects.get(slug=category_name_slug)
     except Category.DoesNotExist:
         category = None
-        form = PageForm()
-        if request.method == 'POST':
-            form = PageForm(request.POST)
-            if form.is_valid():
-                if category:
-                    page = form.save(commit=False)
-                    page.category = category
-                    page.views = 0
-                    page.save()
-                    return show_category(request, category_name_slug)
-                else:
-                    print(form.errors)
-                    context_dict = {'form':form, 'category': category}
-                    return render(request, 'rango/add_page.html', context_dict)
+    form = PageForm()
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+                return show_category(request, category_name_slug)
+        else:
+            print(form.errors)
+    context_dict = {'form':form, 'category': category}
+    return render(request, 'rango/add_page.html', context_dict)
